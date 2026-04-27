8368ed05a51d9e2002053ff94e1a0f52c017330b
Akashdeep Singh <akah47.singh@gmail.com>
2026-04-27 13:03:58 +0200

    Correcion de error de escritora

--- view/includes/message.php
+++ view/includes/message.php
@@ -72,7 +72,7 @@ if (!empty($errors) || !empty($successes)):
 
             <?php if (!empty($successes)) { ?>
                 <p id="flash-title" class="flash-heading">
-                    ✓ Se han completado correctamente la siguiente accion:
+                    ✓ Se ha completado correctamente la siguiente accion:
                 </p>
                 <ul class="flash-list">
                     <?php foreach ($successes as $msg) { ?>

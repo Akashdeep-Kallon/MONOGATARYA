9a8199e776c6201bed9ce371d8eaef9787380a6c
Akashdeep Singh <akah47.singh@gmail.com>
2026-04-28 13:40:02 +0200

    Refactor visual home icon on navegation

--- view/includes/navegation.php
+++ view/includes/navegation.php
@@ -1,7 +1,7 @@
 <!-- Navegación -->
 <div class="pagination">
     <?php $readPage = strtolower($type) . '-read.php'; ?>
- 
+
     <?php if ($prevId) { ?>
         <a href="<?php echo $readPage; ?>?type=<?php echo $type; ?>&id=<?php echo $id; ?>&idChapter=<?php echo $prevId; ?>&numberChapter=<?php echo $prevChapter; ?>">
             &laquo;
@@ -9,11 +9,11 @@
     <?php } else { ?>
         <span class="pagination-disabled">&laquo;</span>
     <?php } ?>
- 
+
     <a href="../work-detail.php?type=<?php echo $type; ?>&id=<?php echo $id; ?>">
-        <svg aria-label="Inicio" role="img"><use href="/view/assets/img/icon-sprites.svg#home"></use></svg>
+        <svg class="icon icon-home" aria-label="Inicio" role="img"><use href="/view/assets/img/icon-sprites.svg#home"></use></svg>
     </a>
- 
+
     <?php if ($nextId) { ?>
         <a href="<?php echo $readPage; ?>?type=<?php echo $type; ?>&id=<?php echo $id; ?>&idChapter=<?php echo $nextId; ?>&numberChapter=<?php echo $nextChapter; ?>">
             &raquo;
